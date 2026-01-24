b396cc774c583eca978d350cb49bfc1c0915d74d
Thomas <thomas.ibanez@epfl.ch>
2026-01-24 15:08:41 +0100

    fix missing include

--- DearImXML/include/XMLTree.h
+++ DearImXML/include/XMLTree.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ImGuiEnum.h>
+#include <imgui.h>
 
 #include <string>
 #include <unordered_map>
@@ -18,8 +19,8 @@ struct XMLNode {
     ImGuiEnum type;
     std::unordered_map<std::string, std::string> args;
     int flags = 0;
-    int childflags = 0; //only if node is "child"
-    ImVec2 size = ImVec2(0, 0);
+    int childflags = 0;  //only if node is "child"
+    ImVec2 size{0, 0};
     bool hide = false;
     std::vector<XMLNode*> children;
     //Empty for non table nodes
